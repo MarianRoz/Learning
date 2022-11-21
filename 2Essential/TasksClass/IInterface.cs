663677e4c6a07f9af8eaa52a2d1df80e4a999432
Marian <rozumnyi.marian@gmail.com>
2022-11-21 22:38:27 +0200

    Add new task IInterface

---
+++ 2Essential/TasksClass/IInterface.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace _2Essential.TasksClass
+{
+//    Задание 3
+//Используя Visual Studio, создайте проект по шаблону Console Application.
+//Требуется:
+//Создайте 2 интерфейса IPlayable и IRecodable. В каждом из интерфейсов создайте по 3 метода void Play() / void Pause() / void Stop() и void Record() / void Pause() / void Stop() соответственно.
+//Создайте производный класс Playerот базовых интерфейсов IPlayable и IRecodable.
+//Написать программу, которая выполняет проигрывание и запись.
+
+    interface IPlayable
+    {
+        void Play();
+        void Pause();
+        void Stop();
+    }
+    interface IRecodable
+    {
+        void Record();
+        void Pause();
+        void Stop();
+    }
+
+    class BaseClassPlayable : IPlayable
+    {
+        public void Play()
+        {
+            Console.WriteLine("Play in Playable");
+        }
+        public void Pause()
+        {
+            Console.WriteLine("Pause in Playable");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Stop in Playable");
+        }
+    }
+    class BaseClassRecodable : IRecodable
+    {
+        public void Record()
+        {
+            Console.WriteLine("Record in Recodable");
+        }
+        public void Pause()
+        {
+            Console.WriteLine("Pause in Recodable");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Stop in Recodable");
+        }
+    }
+}
